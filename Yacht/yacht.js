9baa344fd2f2614c96796f0ab4b1b80bdc9170be
Zayne Hussain <78383186+Shulker125@users.noreply.github.com>
2021-06-09 13:48:30 -0700

    started on nav bar script

--- Yacht/yacht.js
+++ Yacht/yacht.js
@@ -1,1 +1,21 @@
+function topFunction() {
+  
+  document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+  
+}
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+    
+  document.getElementById("rtrntp").style.display = "block";
+  
+  else {
+      document.getElementById('rtrntp').style.display = "none";
+    }
+  
+  
+}
